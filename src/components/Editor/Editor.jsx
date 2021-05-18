9f22ee8ba33751b407908d9accac827eeed8d7b2
Natalie Enkin <natalie@enkin.co.il>
2021-05-18 12:14:50 +0300

    separating the editor component to a separate file

---
+++ src/components/Editor/Editor.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Editor({ title, lines, children }) {
+  //console.log(children);
+  return (
+    <div
+      className={`editor editor__${
+        title && title.toLowerCase().split(" ").join("-")
+      }`}
+    >
+      <h2 className="editor_title">{title}:</h2>
+      {children}
+    </div>
+  );
+}
