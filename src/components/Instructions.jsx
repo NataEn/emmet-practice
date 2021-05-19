fed259ba3ea9a3f298dfe3cc7f1178dea43e195a
Natalie Enkin <natalie@enkin.co.il>
2021-05-19 23:03:44 +0300

    styling game instructions with styled component

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+const StyledInstructions = styled.div`
+  border: 1px solid black;
+  border-radius: ${(props) => props.theme.border.radius};
+  grid-area: instructions;
+  overflow: hidden;
+  text-align: left;
+`;
 
 export default function Instructions(instructions) {
   return (
-    <div className="instructions">
+    <StyledInstructions>
       <p>
         here are the instructions for the exercise
         {!!instructions}
       </p>
-    </div>
+    </StyledInstructions>
   );
 }
